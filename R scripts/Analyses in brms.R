61f3ecc74c9b725234b0011f3f04b4d71d0e399d
Tor Hansson Frank <torhf@ntnu.no>
2025-11-26 16:00:29 +0100

    Updated BRMS-models scripts

--- R scripts/Analyses in brms.R
+++ R scripts/Analyses in brms.R
@@ -11,12 +11,72 @@ library(writexl)
 
 adult_fitness<-read.table("Data/cleaned_density_dependence_data.txt", sep=";", header=TRUE)
 nestling_producers <- read.table("Data/nestling_producers_2007_2014.txt", sep = ";")
-growth_factors<-read.table("Data/growth_factors.txt", sep = ";")
+demo <-read.table("Data/estimated_pop_sizes_nestling_to_adult_model_with_lurøy_onøy.txt", sep =";", header = T, encoding = "UTF-8")
+
+demo_new_sub<-demo %>% 
+  dplyr::select(Location, Year, N_corr) %>% 
+  mutate(N_corr = round(N_corr))
+
+adult_fitness<-adult_fitness %>% 
+  left_join(demo_new_sub)
+
+adult_fitness %>% 
+  filter(is.na(N_corr))
+
+island_N_corr_means<-adult_fitness %>% 
+  dplyr::select(Year, Location, N_corr) %>% 
+  unique() %>% 
+  group_by(Location) %>% 
+  summarise(Mean_N_corr_isl = mean(N_corr)) %>% 
+  ungroup()
+
+#Add the means back to the dataset
+adult_fitness<- adult_fitness %>% 
+  left_join(island_N_corr_means, by ="Location")
+
+island_N_corr_means %>% 
+  arrange(desc(Mean_N_corr_isl))
+
+adult_fitness<-adult_fitness %>% 
+  mutate(mc_N_corr = N_corr - Mean_N_corr_isl)
+
+scaler <- 40 
+
+adult_fitness<- adult_fitness %>% 
+  mutate(zNR_new = mc_N_corr/scaler)
+
+adult_fitness$n_new <- log(adult_fitness$N_corr)
+
+nestling_producers<-nestling_producers %>% 
+  left_join(demo_new_sub)
+
+
+island_N_corr_means2<-nestling_producers %>% 
+  dplyr::select(Year, Location, N_corr) %>% 
+  unique() %>% 
+  group_by(Location) %>% 
+  summarise(Mean_N_corr_isl = mean(N_corr)) %>% 
+  ungroup()
+
+#Add the means back to the dataset
+nestling_producers<- nestling_producers %>% 
+  left_join(island_N_corr_means2, by ="Location")
+
+island_N_corr_means2 %>% 
+  arrange(desc(Mean_N_corr_isl))
+
+nestling_producers<-nestling_producers %>% 
+  mutate(mc_N_corr = N_corr - Mean_N_corr_isl)
+
+scaler <- 40 
+
+nestling_producers<- nestling_producers %>% 
+  mutate(zNR_new = mc_N_corr/scaler)
 
 #Set the recruit year as 0.
-adult_fitness<- adult_fitness %>% 
-  mutate(Least_age = Least_age-1) %>% 
-  mutate(Least_age_sq = Least_age^2)
+adult_fitness<-adult_fitness %>% 
+  mutate(Least_age_mc = Least_age-mean(Least_age)) %>% 
+  mutate(Least_age_mc_sq = Least_age_mc^2)
 
 x<-adult_fitness %>% 
   dplyr::select(Location, in_out) %>% 
@@ -24,6 +84,12 @@ x<-adult_fitness %>%
   summarise(sum(in_out))
 
 stopifnot(x==6)
+
+nestling_producers<-nestling_producers %>% 
+  mutate(Least_age_mc = Least_age-mean(Least_age)) %>% 
+  mutate(Least_age_mc_sq = Least_age_mc^2)
+
+
 
 #r2S-models####
 
@@ -34,18 +100,18 @@ bs_prior <- c(set_prior("normal(0,2)", class = "b"),
               set_prior("normal(0,1)", class = "sd")) #You cannot put lb (lower bound) or ub(upper bound) on SD's in BRMS. But the lower bound is 0 by default.
 
 #fit a model without the inout interaction with density.
-bernt_zNR_nb_brm_no_in_out_slope<-brm(w ~ zNR + in_out + Least_age_sq+ Least_age  * sex + (1|ID) + (1+zNR|Location)
-                                      + (1|Year_Location),adult_fitness,iter = 25000,thin = 30,
+r2s_zNR_new_no_inout<-brm(w ~ zNR_new + in_out + Least_age_mc+ Least_age_mc_sq  * sex + (1|ID) + (1+zNR_new|Location)
+                                      + (1|Year_Location),adult_fitness,iter = 8000,thin = 2,
                                       cores = 4, negbinomial(link = "log", link_shape = "log"))
 
-saveRDS(bernt_zNR_nb_brm_no_in_out_slope, file("Workspace backup/bernt_zNR_nb_brm_no_in_out_slope.rds"))
+saveRDS(r2s_zNR_new_no_inout, file("Workspace backup/r2s_zNR_new_no_inout.rds"))
 
 #fit main model
-bernt_zNR_nb_brm_noindslope<-brm(w ~ zNR*in_out + Least_age_sq+ Least_age  * sex + (1|ID) + (1+zNR|Location)
-                                 + (1|Year_Location),adult_fitness,iter = 25000,thin = 30,
+r2s_zNR_new<-brm(w ~ zNR_new*in_out + Least_age_mc+ Least_age_mc_sq  * sex + (1|ID) + (1+zNR_new|Location)
+                                 + (1|Year_Location),adult_fitness,iter = 8000,thin = 2,
                                  cores = 4, negbinomial(link = "log", link_shape = "log"))
 
-saveRDS(bernt_zNR_nb_brm_noindslope, file("Workspace backup/bernt_zNR_nb_brm_noindslope.rds"))
+saveRDS(r2s_zNR_new, file("Workspace backup/r2s_zNR_new.rds"))
 
 ##single island models####
 
@@ -62,12 +128,12 @@ single_island_analysis<-function(adult_fitness,distribution,ni,nt,nc,bs_prior){
     ind_isl_data <- subset(adult_fitness, Location == ind_islands$Location[i])
     
     if(distribution == "negbin"){
-      ind_isl_mod <- brm(w ~ gamma +Least_age_sq+ Least_age * sex
+      ind_isl_mod <- brm(w ~ gamma +Least_age_mc+ Least_age_mc_sq * sex
                          + (1|ID) 
                          + (1|Year),ind_isl_data, iter = ni,thin = nt,chains = nc,
                          cores = nc, prior = bs_prior, negbinomial(link = "log", link_shape = "log"))
     } else if(distribution == "bernoulli"){
-      ind_isl_mod <- brm(w ~ gamma + Least_age_sq+Least_age  * sex
+      ind_isl_mod <- brm(w ~ gamma + Least_age_mc+Least_age_mc_sq  * sex
                          + (1|ID) 
                          + (1|Year),ind_isl_data, iter = ni,thin = nc,chains =nc,
                          cores = nc, prior = bs_prior, bernoulli(link = "logit"))
@@ -81,34 +147,26 @@ single_island_analysis<-function(adult_fitness,distribution,ni,nt,nc,bs_prior){
 bs_prior <- c(set_prior("normal(0,2)", class = "b"),
               set_prior("normal(0,1)", class = "sd")) #You cannot put lb (lower bound) or ub(upper bound) on SD's in BRMS. But the lower bound is 0 by default.
 
-adult_fitness$gamma<-adult_fitness$zNR
+adult_fitness$gamma<-adult_fitness$zNR_new
 distribution = "negbin"
-ni = 12000
-nt = 30
+ni = 6000
+nt = 2
 nc = 4
 
 results_single_islands<-single_island_analysis(adult_fitness,distribution,ni,nt,nc,bs_prior)
 
 
-saveRDS(results_single_islands, file("Workspace backup/bernt_zNR_single_island_mods.rds"))
+saveRDS(results_single_islands, file("Workspace backup/r2s_zNR_single_island_mods_NEW.rds"))
 
 ##log N model ####
 
 bs_prior <- c(set_prior("normal(0,2)", class = "b"),
               set_prior("normal(0,1)", class = "sd")) #You cannot put lb (lower bound) or ub(upper bound) on SD's in BRMS. But the lower bound is 0 by default.
 
-#fit a model without the inout interaction with density.
-bernt_n_nb_brm_no_in_out_slope<-brm(w ~ n+in_out + Least_age_sq + Least_age * sex + (1|ID) + (1+n|Location)
-                                    + (1|Year_Location),adult_fitness,iter = 12000,thin = 30,
-                                    cores = 4, prior = bs_prior ,negbinomial(link = "log", link_shape = "log"))
-saveRDS(bernt_n_nb_brm_no_in_out_slope, file("Workspace backup/bernt_n_nb_brm_no_in_out_slope.rds"))
-
-
-#main model
-bernt_n_nb_brm_noindslope<-brm(w ~ n*in_out + Least_age_sq + Least_age * sex + (1|ID) + (1+n|Location)
-                               + (1|Year_Location),adult_fitness,iter = 12000,thin = 30,
+r2s_n_new<-brm(w ~ n_new*in_out + Least_age_mc + Least_age_mc_sq * sex + (1|ID) + (1+n_new|Location)
+                               + (1|Year_Location),adult_fitness,iter = 4000,thin = 2,
                                cores = 4, prior = bs_prior ,negbinomial(link = "log", link_shape = "log"))
-saveRDS(bernt_n_nb_brm_noindslope, file("Workspace backup/bernt_n_nb_brm_noindslope.rds"))
+saveRDS(r2s_n_new, file("Workspace backup/r2s_n_new.rds"))
 
 #Recruit and nestling production models ####
 ##recruit N_m ####
@@ -118,10 +176,10 @@ adult_fitness$w<-adult_fitness$N_Recruits
 bs_prior <- c(set_prior("normal(0,2)", class = "b"),
               set_prior("normal(0,1)", class = "sd"))#You cannot put lb (lower bound) or ub(upper bound) on SD's in BRMS. But the lower bound is 0 by default.
 
-rec_zNR_nb_brm_noindslope<-brm(w ~ zNR*in_out + Least_age_sq + Least_age * sex+ (1|ID) + (1+zNR|Location)
-                               + (1|Year_Location), adult_fitness,iter = 25000,thin = 30, cores = 4,prior=bs_prior, negbinomial(link = "log", link_shape = "log"))
+N_recruits_zNR_new<-brm(w ~ zNR_new*in_out + Least_age_mc + Least_age_mc_sq * sex+ (1|ID) + (1+zNR_new|Location)
+                               + (1|Year_Location), adult_fitness,iter = 4000,thin = 2, cores = 4,prior=bs_prior, negbinomial(link = "log", link_shape = "log"))
 
-saveRDS(rec_zNR_nb_brm_noindslope, file("Workspace backup/rec_zNR_nb_brm_noindslope.rds"))
+saveRDS(N_recruits_zNR_new, file("Workspace backup/N_recruits_zNR_new.rds"))
 
 ##nestling production N_m ####
 
@@ -129,80 +187,8 @@ bs_prior <- c(set_prior("normal(0,2)", class = "b"),
               set_prior("normal(0,1)", class = "sd"))
 
 
-nestlings_zNR_nb_brm_noindslope<-brm(N_nestlings ~ zNR*in_out + Least_age_sq + Least_age * sex + (1|ID) + (1+zNR|Location)
-                                     + (1|Year_Location),nestling_producers,iter = 12000,thin = 30,
-                                     cores = 4, prior = bs_prior, negbinomial(link = "log", link_shape = "log"))
-saveRDS(nestlings_zNR_nb_brm_noindslope, file("Workspace backup/nestlings_zNR_nb_brm_noindslope.rds"))
+nestlings_zNR_nb_brm_noindslope_new<-brm(N_nestlings ~ zNR_new*in_out + Least_age_mc_sq + Least_age_mc * sex + (1|ID) + (1+zNR_new|Location)
+                                         + (1|Year_Location),nestling_producers,iter = 6000,thin = 6,
+                                         cores = 4, prior = bs_prior, negbinomial(link = "log", link_shape = "log"))
+saveRDS(nestlings_zNR_nb_brm_noindslope_new, file("Workspace backup/nestlings_zNR_nb_brm_noindslope_new.rds"))
 
-#Sensitivities####
-
-#remove NA rows
-growth_factors<-na.omit(growth_factors)
-
-
-#remove rows with 0
-growth_factors<-growth_factors %>% 
-  filter(!if_any(c(8:11),~ . < 1))
-
-
-growth_factors<-growth_factors %>% 
-  mutate(across(8:11, log))
-
-growth_factors<-growth_factors %>% 
-  filter(lambda<4)
-
-growth_factors<-growth_factors %>% 
-  mutate(n = log(N))
-
-bs_prior <- c(set_prior("normal(0,2)", class = "b"),
-              set_prior("normal(0,1)", class = "sd"))
-
-
-growth_mod_lambda_brm<-brm(lambda ~ n + Adult_survival + 
-                             Fledgling_to_rec + 
-                             Nestling_to_fledge + 
-                             Nestlings_born +
-                             (1|Year) + (n|Location), 
-                           data = growth_factors,
-                           family = Gamma(link = "log"),
-                           prior = bs_prior,
-                           iter = 60000,
-                           thin = 60,
-                           cores = 4)
-saveRDS(growth_mod_lambda_brm, "Workspace backup/growth_mod_lambda_brm.rds")
-
-growth_mod_lambda_brm_inout<-brm(lambda ~ n + 
-                                   in_out * Adult_survival + 
-                                   in_out * Fledgling_to_rec + 
-                                   in_out * Nestling_to_fledge + 
-                                   in_out * Nestlings_born +
-                                   (1|Year) + (n|Location), 
-                                 data = growth_factors,
-                                 family = Gamma(link = "log"),
-                                 prior = bs_prior,
-                                 iter = 60000,
-                                 thin = 60,
-                                 cores = 4)
-saveRDS(growth_mod_lambda_brm_inout, "Workspace backup/growth_mod_lambda_brm_inout.rds")
-
-
-max_ages<- nestling_producers %>% 
-  group_by(ID) %>% 
-  mutate(Least_age = Least_age + 1) %>% 
-  mutate(max_age = max(Least_age)) %>% 
-  mutate(lrs = sum(N_nestlings)) %>% 
-  mutate(nestlings_per_year = lrs/max_age) %>% 
-  dplyr::select(ID, Location, max_age, nestlings_per_year, lrs, sex) %>% 
-  unique()
-
-descdist(max_ages$max_age, boot = 500, discrete = T)
-
-age_mod_brm<-brm(max_age~nestlings_per_year + (1|Location),
-                 data = max_ages,
-                 family = poisson,
-                 prior = bs_prior,
-                 iter = 20000,
-                 thin = 30,
-                 cores = 4)
-
-saveRDS(age_mod_brm, "Workspace backup/max_age_yearly_nestlingprod_brm.rds")
